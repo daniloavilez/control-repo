8e5efec49fa9412ecf6fae0395bd82b8a2a02663
Danilo Avilez <dgavilez@gmail.com>
2019-03-10 22:50:14 +0000

    Configuration for dev environment

--- manifests/app.pp
+++ manifests/app.pp
@@ -1,4 +1,4 @@
-node 'node1' {
+node 'node3' {
   
   include base
 
@@ -15,35 +15,30 @@ node 'node1' {
 
 }
 
-node 'node2' {
 
-  include base
 
-  class { '::tomcat': 
-    user => 'tomcat',
-    group => 'tomcat',
-    service_state => 'running'
-  }
 
-  tomcat::deploy { "sysfoo":
-    deploy_url => 'https://11-174440744-gh.circle-artifacts.com/0/home/circleci/repo/target/sysfoo.war',
-    checksum_value => '0ba945b10be51e5d15cf0a178245727a',
-  }
+
+
+
+
+    
   
-}
+ 
 
-node default {
-  
-  notify { 'checkpoint_1': 
-    message => "
 
-      CHECKPOINT_!
 
-      Applying default BLOCK
-      Looks like there is no definition for this node.... 
-
-    "
-  } 
   
 
-}
+
+
+
+
+
+
+
+
+
+  
+
+
